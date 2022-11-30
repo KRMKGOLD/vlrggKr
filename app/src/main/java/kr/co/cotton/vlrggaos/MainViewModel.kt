8cbf5ac97567616a0cfd677c29eea6c52027099b
KRMKGOLD <dkqk0124@naver.com>
2022-12-01 01:43:33 +0900

    Main Activity, VM Hilt 적용

--- app/src/main/java/kr/co/cotton/vlrggaos/MainViewModel.kt
+++ app/src/main/java/kr/co/cotton/vlrggaos/MainViewModel.kt
@@ -1,7 +1,10 @@
 package kr.co.cotton.vlrggaos
 
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kr.co.cotton.vlrggaos.base.BaseViewModel
+import javax.inject.Inject
 
-class MainViewModel : BaseViewModel() {
+@HiltViewModel
+class MainViewModel @Inject constructor() : BaseViewModel() {
 
 }
