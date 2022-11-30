7019deb95471bccbd0ec1fdf66a8a3e7f09e85ce
KRMKGOLD <dkqk0124@naver.com>
2022-11-30 21:30:09 +0900

    Compose 프로젝트로 수정

--- app/src/main/java/kr/co/cotton/vlrggaos/MainActivity.kt
+++ app/src/main/java/kr/co/cotton/vlrggaos/MainActivity.kt
@@ -1,11 +1,27 @@
 package kr.co.cotton.vlrggaos
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import kr.co.cotton.vlrggaos.ui.theme.VlrggKrTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            VlrggKrTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
     }
 }
