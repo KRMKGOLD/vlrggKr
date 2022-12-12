e312e01a63e7fa4bd7e626e89201a817030a5de2
KRMKGOLD <dkqk0124@naver.com>
2022-12-12 21:50:16 +0900

    href + url 추가

--- core/data/src/main/java/kr/co/cotton/data/sportsnews/ValEsportsNews.kt
+++ core/data/src/main/java/kr/co/cotton/data/sportsnews/ValEsportsNews.kt
@@ -1,9 +1,13 @@
 package kr.co.cotton.data.sportsnews
 
 data class ValEsportsNews(
+    val href: String? = null,
     val title: String? = null,
     val description: String? = null,
     val flagISO: String? = null,
     val date: String? = null,
     val writer: String? = null
-)
+) {
+
+    val url: String? = "https://www.vlr.ggv${href}"
+}
