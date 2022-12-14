349cda615932bf9b9e9850e736ccff66f4a22ce1
KRMKGOLD <dkqk0124@naver.com>
2022-12-14 20:56:41 +0900

    CottonTopBar 개발

---
+++ core/designsystem/src/main/java/kr/co/cotton/designsystem/component/CottonTopBar.kt
@@ -0,0 +1,51 @@
+package kr.co.cotton.designsystem.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CottonTopBar(
+    title: String,
+    onClickBackBtn: (() -> Unit)? = null
+) {
+    TopAppBar(
+        elevation = 4.dp,
+        title = {
+            Text(text = title)
+        },
+        backgroundColor = MaterialTheme.colors.primarySurface,
+        navigationIcon = {
+            if (onClickBackBtn != null) {
+                IconButton(
+                    onClick = { onClickBackBtn() }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = null
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CottonTopBarPreview() {
+    CottonTopBar("Test Title")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CottonTopBarPreview2() {
+    CottonTopBar(
+        title = "Test Title",
+        onClickBackBtn = { }
+    )
+}
