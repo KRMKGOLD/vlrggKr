ddee1559cb2fa6dd1fb1beaa955265fe9f0fb02f
KRMKGOLD <dkqk0124@naver.com>
2022-12-29 22:01:26 +0900

    SearchResult 구조 변경

--- core/data/src/main/java/kr/co/cotton/core/data/search/model/SearchResult.kt
+++ core/data/src/main/java/kr/co/cotton/core/data/search/model/SearchResult.kt
@@ -1,31 +1,42 @@
 package kr.co.cotton.core.data.search.model
 
-
 sealed class SearchResult(
-    val imgSrc: String? = null,
-    val href: String? = null,
-    val url: String? = null
+    open val imgSrc: String?,
+    open val href: String?,
+    open val url: String?
 ) {
 
     data class SearchTeam(
+        override val imgSrc: String? = null,
+        override val href: String? = null,
+        override val url: String? = null,
         val name: String? = null,
         val inactiveStr: String? = null,
         val previouslyStr: String? = null,
-        val currentlyStr: String? = null
-    ) : SearchResult()
+        val currentlyStr: String? = null,
+    ) : SearchResult(imgSrc, href, url)
 
     data class SearchPlayer(
+        override val imgSrc: String? = null,
+        override val href: String? = null,
+        override val url: String? = null,
         val nickname: String? = null,
         val realName: String? = null,
-    ) : SearchResult()
+    ) : SearchResult(imgSrc, href, url)
 
     data class SearchEvent(
+        override val imgSrc: String? = null,
+        override val href: String? = null,
+        override val url: String? = null,
         val title: String? = null,
         val eventPeriod: String? = null,
         val prizePool: String? = null
-    ) : SearchResult()
+    ) : SearchResult(imgSrc, href, url)
 
     data class SearchSeries(
+        override val imgSrc: String? = null,
+        override val href: String? = null,
+        override val url: String? = null,
         val title: String? = null
-    ) : SearchResult()
+    ) : SearchResult(imgSrc, href, url)
 }
