d1ece3782094e10927a184bfd2c2eacd93db93fe
KRMKGOLD <dkqk0124@naver.com>
2023-01-08 23:43:50 +0900

    CottonErrorView 추가

---
+++ core/designsystem/src/main/java/kr/co/cotton/core/designsystem/component/common/CottonErrorView.kt
@@ -0,0 +1,57 @@
+package kr.co.cotton.core.designsystem.component.common
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kr.co.cotton.core.designsystem.component.theme.CottonTheme
+
+@Composable
+fun CottonErrorView(
+    modifier: Modifier = Modifier,
+    errorIcon: ImageVector = Icons.Outlined.Info,
+    errorTitle: String = "Error",
+    errorMessage: String = "Please Try Again",
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            modifier = Modifier.size(120.dp),
+            imageVector = errorIcon,
+            contentDescription = null,
+            tint = MaterialTheme.colors.error
+        )
+        Text(
+            modifier = Modifier.padding(top = 16.dp),
+            text = errorTitle,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            modifier = Modifier.padding(top = 16.dp),
+            text = errorMessage,
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CottonErrorViewPreview() {
+    CottonTheme {
+        CottonErrorView()
+    }
+}
