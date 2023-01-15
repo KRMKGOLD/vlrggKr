b06f1839f3fbd17e6e1757fadd6c1a1b130e21f7
KRMKGOLD <dkqk0124@naver.com>
2023-01-15 23:59:47 +0900

    import 정리

--- feature/search/src/main/java/kr/co/cotton/feature/search/view/SearchPlayerView.kt
+++ feature/search/src/main/java/kr/co/cotton/feature/search/view/SearchPlayerView.kt
@@ -5,10 +5,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
