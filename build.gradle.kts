00f310bd5f5684d95417a2052cbdddcdf3240218
KRMKGOLD <dkqk0124@naver.com>
2023-02-07 23:47:04 +0900

    [VLR-6] ktlint 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,17 @@
+plugins {
+    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
+    id("org.jlleitschuh.gradle.ktlint-idea") version "11.0.0"
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:11.0.0")
+    }
+}
+
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
