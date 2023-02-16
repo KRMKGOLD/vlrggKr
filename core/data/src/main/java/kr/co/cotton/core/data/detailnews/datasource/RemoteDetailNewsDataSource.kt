69aa8a2c00d66ee0a3be70b0a844ac19dd013b95
KRMKGOLD <dkqk0124@naver.com>
2023-02-16 23:50:44 +0900

    [VLR-7] HTML에서 Header 분리 및 title, writer 분리

--- core/data/src/main/java/kr/co/cotton/core/data/detailnews/datasource/RemoteDetailNewsDataSource.kt
+++ core/data/src/main/java/kr/co/cotton/core/data/detailnews/datasource/RemoteDetailNewsDataSource.kt
@@ -2,12 +2,31 @@ package kr.co.cotton.core.data.detailnews.datasource
 
 import org.jsoup.Jsoup
 import javax.inject.Inject
+import org.jsoup.select.Elements
 
 class RemoteDetailNewsDataSource @Inject constructor() : DetailNewsDataSource {
 
     override fun getDetailNews(nId: String, title: String): String {
         val url = "https://www.vlr.gg/$nId/$title"
         val doc = Jsoup.connect(url).get()
-        return doc.select("div.wf-card.mod-article").html()
+        val article = doc.select("div.wf-card.mod-article")
+        val header = article.select("div.article-header")
+        val body = article.select("div.article-body")
+
+        val (title, writer) = getHeaderList(header)
+        getBodyList(body)
+
+        return ""
+    }
+
+    private fun getHeaderList(header: Elements): Pair<String, String> {
+        val title = header.select("h1.wf-title").text()
+        val writer = header.select("a.article-meta-author").text()
+
+        return title to writer
+    }
+
+    private fun getBodyList(body: Elements) {
+
     }
 }
